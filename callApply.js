aa58a422b863f2c196cb2546c46bb203f0667c53
rokydas <droky136@gmail.com>
2020-08-11 19:08:25 +0600

    callApply added

--- callApply.js
+++ callApply.js
@@ -5,8 +5,8 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
-        this.salary = this.salary - amount;
+    chargeBill: function(amount, tax){
+        this.salary = this.salary - amount - tax;
         return this.salary;
     }
 }
@@ -24,11 +24,25 @@ const friendlyPerson = {
 } 
 
 const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroChargeBill(2000);
+heroChargeBill(2000, 100);
 console.log(heroPerson.salary);
 
 console.log(normalPerson);
 console.log(normalPerson.firstName);
-normalPerson.chargeBill(150);
-normalPerson.chargeBill(1050);
+normalPerson.chargeBill(150, 100);
+normalPerson.chargeBill(1050, 100);
 console.log(normalPerson.salary);
+
+// arekjoner patil niye eshe nije ranna kora ==> bind
+// or arekjoner boi dhar kore ene pass kora ==> bind
+
+normalPerson.chargeBill.call(heroPerson, 900, 100);
+console.log(heroPerson.salary);
+
+// sorasori or basai giye or mobile diye nijer number diye dial kora ==> call
+
+// call ar apply er difference holo object er pore je value gula pathabo call e segula comma diye diye pathabo
+// ar apply e object er por value gula ekta array te niye tarpor pathabo. 
+
+normalPerson.chargeBill.apply(heroPerson, [900, 100]);
+console.log(heroPerson.salary);
