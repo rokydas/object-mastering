6917161e92796a82625473fd68df37b1484f7ad5
rokydas <droky136@gmail.com>
2020-08-12 00:02:28 +0600

    interval added

---
+++ interval.js
@@ -0,0 +1,16 @@
+
+console.log(2222);
+// doSomething();
+
+// using arrow function
+setInterval(() => {
+    console.log("See you later");
+}, 3000);
+// To stop this press ctrl + c
+
+// setTimeout function er kaj sob kaj sesh howar por korbe. 
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
