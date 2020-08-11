37a0eea71a34ae915731516940e6882a8dccfcc7
rokydas <droky136@gmail.com>
2020-08-11 23:57:24 +0600

    timeout added

---
+++ timeout.js
@@ -0,0 +1,19 @@
+
+console.log(2222);
+// doSomething();
+
+setTimeout(function() {
+    console.log(3333);
+}, 2000); // 2 sec por kaj korbe. 
+
+// using arrow function
+setTimeout(() => {
+    console.log("See you later");
+}, 10000);
+
+// setTimeout function er kaj sob kaj sesh howar por korbe. 
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
